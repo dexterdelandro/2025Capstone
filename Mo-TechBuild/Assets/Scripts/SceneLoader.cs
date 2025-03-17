6f076ab850ce0529d9377c00456d834a465b6d10
KeithZhu1030 <sddzzjz1030@gmail.com>
2025-03-17 03:14:44 -0700

    commit

---
+++ Mo-TechBuild/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+public class SceneLoader : MonoBehaviour
+{
+    private void Update()
+    {
+       
+    }
+
+    private void LoadNextScene()
+    {
+       
+    }
+
+    public void jump() {
+        SceneManager.LoadScene("Level GDC");
+    }
+}
