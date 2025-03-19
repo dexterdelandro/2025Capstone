a2c60aa19081520e038768b7bc9f2a07966a580c
KeithZhu1030 <sddzzjz1030@gmail.com>
2025-03-19 13:16:16 -0700

    commit

---
+++ Mo-TechBuild/Assets/ToggleObjects.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class ToggleObjects : MonoBehaviour
+{
+    public GameObject objectToDisable; // 需要关闭的对象
+    public GameObject objectToEnable;  // 需要开启的对象
+
+    void Update()
+    {
+        // 检测是否按下了手柄的X键 (Xbox手柄通常是 "joystick button 2")
+        if (Input.GetKeyDown(KeyCode.JoystickButton8))
+        {
+            Toggle();
+        }
+    }
+
+    void Toggle()
+    {
+        // 切换对象的激活状态
+        if (objectToDisable != null)
+        {
+            objectToDisable.SetActive(false);
+        }
+
+        if (objectToEnable != null)
+        {
+            objectToEnable.SetActive(true);
+        }
+    }
+}
